92aa713b857af92ce8d24c7ffb7f3be6e6cc31ef
TarasekM <mateusz.tarasek@smcebi.edu.pl>
2018-07-18 12:02:57 +0200

    22 jquery

--- customers/customers.js
+++ customers/customers.js
@@ -1,6 +1,10 @@
 var images = document.querySelectorAll('.testimonial__image-container');
 var text = document.querySelectorAll('.testimonial__info');
 
+for(var i = 0; i < 2; i++){
+  text[i].style.width = images[i].offsetWidth + 'px';
+}
+
 $(window).resize(function(){
   for(var i = 0; i < 2; i++){
     text[i].style.width = images[i].offsetWidth + 'px';
